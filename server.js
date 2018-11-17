2181710c9a802a2e78d97121b40da1123e346f7f
semreg <vchabaniuk@outlook.com>
2018-11-17 18:22:26 +0200

    Structure

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const path = require('path')
 const port = process.env.PORT || 5000
 
-app.use( express.static( `${__dirname}/build` ))
+app.use(express.static(`${__dirname}/build`))
 
 app.get('*', (req, res)=>{
   res.sendFile(path.join(__dirname, './build/index.html'));
